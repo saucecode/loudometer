c65789373315f6476c9af156a3e99a7623f69438
Julian Cahill <cahill.julian@gmail.com>
2023-05-25 19:25:04 +1200

    Parallelized the listening to operate on a per-channel basis. Modified the config file to have a http target for each channel Main loop will split input data into channel data, and calculate volumes per channel. Main loop will monitor loudness triggers per channel, and each channel has a unique http target, as set in the config file

--- loudometer.py
+++ loudometer.py
@@ -20,6 +20,8 @@ import wave
 import sys
 
 import os, json, time, requests, threading
+import io
+import itertools
 import logging as log
 
 import pyaudio
@@ -31,13 +33,16 @@ log.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:%M:%
 
 def generate_config():
 	with open('config.json', 'w') as f:
-		json.dump({
-			'http_target': 'http://127.0.0.1:8888/press/bank/1/2',
+		template = {
 			'print_volume_every_second': True,
 			'volume_trigger_threshold': 1000,
 			'minimum_time_between_triggers_in_seconds': 10,
 			"active": True
-		}, f, indent=4)
+		}
+		for i in range(32):
+			template[f'http_target_channel{i}'] = ''
+		template['http_target_channel0'] = 'http://127.0.0.1:8888/press/bank/1/2'
+		json.dump(template, f, indent=4)
 
 def load_config():
 	with open('config.json', 'r') as f:
@@ -59,9 +64,11 @@ config = load_config()
 p = pyaudio.PyAudio()
 info = p.get_host_api_info_by_index(0)
 numdevices = info.get('deviceCount')
+device_data = {}
 
 for i in range(numdevices):
 	if (device_info := p.get_device_info_by_host_api_device_index(0, i)).get('maxInputChannels') >= 0:
+		device_data[i] = device_info
 		print("Device ID ", i, " - ", device_info.get('name'), '(has',device_info.get('maxInputChannels', 'ERROR'),'channels)')
 
 try:
@@ -70,26 +77,41 @@ except:
 	log.info('Failed!')
 	sys.exit(1)
 
-CHUNK = 1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 1 if sys.platform == 'darwin' else 2
+CHANNELS = device_data[index]['maxInputChannels']
+CHUNK = 1024
 RATE = 44100
 
 p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, input_device_index=index)
 
-log.info('Starting to listen!')
+log.info(f'Starting to listen on {CHANNELS} channels')
 
-largest = 0
+largest = [0] * CHANNELS
 ticker = time.time()
-last_request_sent = 0
+last_request_sent = [0] * CHANNELS
 
 config_poll = time.time()
 config_age = os.stat('config.json').st_mtime
 
 while 1:
-	data = stream.read(CHUNK)
-	volume = audioop.rms(data,2)
+	raw = stream.read(CHUNK)
+	data = io.BytesIO(raw)
+	
+	assert FORMAT == pyaudio.paInt16
+	assert len(raw) == CHUNK * 2 * CHANNELS # 2 because Int16 is 2 bytes
+	
+	# stream data is interlaced by channel.
+	# this will run through the whole chunk and copy the samples to their respective channels
+	channels = [io.BytesIO() for _ in range(CHANNELS)]
+	channels_selector = itertools.cycle(channels)
+	
+	while short := data.read(2):
+		next(channels_selector).write(short)
+	
+	# calculates the volume per channel
+	volumes = [audioop.rms(data.getvalue(), 2) for data in channels]
+	volume = volumes[0]
 	
 	# check for changes in the config file, and load them
 	if time.time() > config_poll + 1:
@@ -102,18 +124,22 @@ while 1:
 	
 	# printing of the recorded volume
 	if config['print_volume_every_second']:
-		largest = max(volume, largest)
+		largest = [max(volume, largest[channel]) for channel, volume in enumerate(volumes)]
 		if time.time() > ticker + 1:
 			ticker = time.time()
-			log.info(f'Highest Volume in the past second: {largest}')
-			largest = 0
+			log.info(f'Highest Volume per channel in the past second: {" ".join(str(i) for i in largest)}')
+			largest = [0] * CHANNELS
 	
-	if volume > config['volume_trigger_threshold'] and time.time() > last_request_sent + config['minimum_time_between_triggers_in_seconds'] and config['active']:
-		target = config['http_target']
-		log.info(f'It\'s loud! Sending request to {target}')
-		# requests.get(target)
-		threading.Thread(target=requests.get, args=(target,)).start()
-		last_request_sent = time.time()
+	for channel, volume in enumerate(volumes):
+		if volume > config['volume_trigger_threshold'] \
+		and time.time() > last_request_sent[channel] + config['minimum_time_between_triggers_in_seconds'] \
+		and config['active'] \
+		and (target := config.get(f'http_target_channel{channel}')):
+			
+			log.info(f'Channel {channel} is loud! Sending request to {target}')
+			# requests.get(target)
+			threading.Thread(target=requests.get, args=(target,)).start()
+			last_request_sent[channel] = time.time()
 
 print('Done')
 
